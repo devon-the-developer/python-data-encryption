92212aefe1bf575ebf39ba63440f21d188fb8525
Micahgard <micahgardner7@gmail.com>
2020-11-12 17:11:22 +1300

    Encrypt plain text and retrieve hash from database

--- decryptDownload.py
+++ decryptDownload.py
@@ -1,7 +1,8 @@
 import json
-from base64 import b64decode
+from base64 import b64decode, b64encode
 from Crypto.Cipher import AES
 from Crypto.Util.Padding import unpad
+from Crypto.Hash import SHA256
 
 import requests
 key = b"Secret byte key "
@@ -21,7 +22,10 @@ def getFromDB(userID):
     return response
 
 def decodePiece(inputVar, cipher):
+    print("decode piece", inputVar, cipher)
     plaintext = unpad(cipher.decrypt(inputVar), AES.block_size).decode()
+    print("plaint text after being decoded", plaintext)
+    print("_________________________")
     return plaintext
 
 def decrypt(jsondata):
@@ -32,19 +36,28 @@ def decrypt(jsondata):
         iv = b64decode(b64['iv'])
         nameEn = b64decode(b64['Name'])
         ageEn = b64decode(b64['Age'])
+        print("encoded age.. ", type(ageEn))
+        hashEn = b64decode(b64['Hash'])
+        print("Encoded hash...", type(hashEn))
+
 
         #set ciphermode and key to decrypt
         cipher = AES.new(key, AES.MODE_CBC, iv)
         name = decodePiece(nameEn, cipher)
         age = decodePiece(ageEn, cipher)
+        print("Decoded age...", type(age))
+        hashPlain = decodePiece(hashEn, cipher)
+        print("decoded hash...", type(hashPlain))
 
         #Comments just to check its working
         print("\nDecrypted Response: \n-----------------")
 
         print("The name is: ", name)
         print("The age is: ", age)
+        #print("The hash is: ", hashPlain)
     except (ValueError, KeyError):
         print("Incorrect Decryption")
+    return (bytes(name, 'utf-8') + bytes(age, 'utf-8'), hashPlain)
 
 def run():
     userID = input("Enter the UserID: ")
@@ -53,4 +66,24 @@ def run():
 
     print("Encrypted Response: ", responsejson[-1])
 
-    decrypt(responsejson)
+    returnedObject = decrypt(responsejson)
+    print(type(returnedObject))
+    ptHash = hash(returnedObject[0])
+    print("2",ptHash)
+    print("3", returnedObject[1])
+    #compareHashes(, ptHash)
+
+def hash(byteText):
+    #print(type(byteText))
+    hash_object = SHA256.new(data=byteText)
+    #hashEn = b64encode(hash_object).decode()
+    #print(hashEn)
+    #print(hash_object.hexdigest())
+    #print(hash_object.digest())
+    return hash_object.hexdigest()
+
+def compareHashes(databaseHash, textHash):
+    if databaseHash == textHash:
+        print("Hashes equal")
+    else:
+        print("Hashes not equal")
--- encryptUpload.py
+++ encryptUpload.py
@@ -2,66 +2,92 @@ import json
 from base64 import b64encode
 from Crypto.Cipher import AES
 from Crypto.Util.Padding import pad
+from Crypto.Hash import SHA256
 from Crypto.Random import get_random_bytes
 
 import requests
 from Crypto import Random
 
+
 def encryptPiece(inputVar, cipher):
-    #pass in data to encrypt and the cipher set with key
+    # pass in data to encrypt and the cipher set with key
     data = inputVar
     ct_bytes = cipher.encrypt(pad(data, AES.block_size))
     return ct_bytes
 
-def aesEncrypt(name, age):
-    #variables must be in bytes - b"-"
+
+def aesEncrypt(name, age, hash):
+    # variables must be in bytes - b"-"
     name = name
     age = age
-    key = b"Secret byte key " #16 bytes long
+    hash = bytes(hash, 'utf-8')
+    print("compare", type(name), type(age), type(hash))
+    key = b"Secret byte key "  # 16 bytes long
 
-    #set cipher to use and encrypt individual data
+    # set cipher to use and encrypt individual data
     cipher = AES.new(key, AES.MODE_CBC)
     ne_bytes = encryptPiece(name, cipher)
     ae_bytes = encryptPiece(age, cipher)
+    hash_bytes = encryptPiece(hash, cipher)
     iv = b64encode(cipher.iv).decode()
 
-    #encoded data is returned in format that can be sent in JSON
+
+    # encoded data is returned in format that can be sent in JSON
     nameEn = b64encode(ne_bytes).decode()
     ageEn = b64encode(ae_bytes).decode()
-    print("result: ",  nameEn, ageEn, iv)
-    return (nameEn, ageEn, iv)
+    hashEn = b64encode(hash_bytes).decode()
+    print("String of encoded hash", hashEn)
+    print("result: ", nameEn, ageEn, iv, hashEn)
+    return (nameEn, ageEn, iv, hashEn)
 
-def sendToDB(data): 
+
+def sendToDB(data):
     url = "https://crypto-ba4d.restdb.io/rest/contacts"
 
-    payload = json.dumps( {"Name": data[0],"Age": data[1], "iv": data[2]} )
+    payload = json.dumps({"Name": data[0], "Age": data[1], "iv": data[2], "Hash": data[3]})
     headers = {
-    'content-type': "application/json",
-    'x-apikey': "a738e377f1dbb542fc42441da180a476685f6",
-    'cache-control': "no-cache"
+        'content-type': "application/json",
+        'x-apikey': "a738e377f1dbb542fc42441da180a476685f6",
+        'cache-control': "no-cache"
     }
 
-    response = requests.request("POST", url, data=payload, headers=headers) 
+    response = requests.request("POST", url, data=payload, headers=headers)
     print(response)
+
 
 def run():
     cusName = input("What is the name of the customer? ")
     cusAge = input("What is their age? ")
-    #Convert strings into bytes
+    # join name and age together as strings
+    hashingString = cusName + cusAge
+    # Convert strings into bytes
     cusName = bytes(cusName, 'utf-8')
     cusAge = bytes(cusAge, 'utf-8')
 
-    #Encrypt given data
-    encryptMsg = aesEncrypt(cusName, cusAge)
+    # convert to bytes
+    bHashString = bytes(hashingString, 'utf-8')
+    print("String to be hashed in bytes",bHashString)
 
-    #Send Encrypted data to DB
+    # hash the result
+    hashOfStrings = hash(bHashString)
+    print("Result of hashing byte string", hashOfStrings)
+    # send that hash to be encoded
+
+    # hashing plain text
+    ptHash = hash(cusName + cusAge)
+    print("hash before encoding.. ", ptHash)
+
+
+    # Encrypt given data
+    encryptMsg = aesEncrypt(cusName, cusAge, hashOfStrings)
+
+    # Send Encrypted data to DB
     sendToDB(encryptMsg)
 
 
-
-
-
-
-
-
+def hash(plainText):
+    hash_object = SHA256.new(data=plainText)
+    print("hexdigest",hash_object.hexdigest())
+    print("digest",hash_object.digest())
+    return hash_object.hexdigest()
 
